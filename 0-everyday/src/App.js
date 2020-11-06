570f5a7f34415fbb6b59cb2ef430bb385d627972
iDeveloperBrandon <hsy0602@outlook.com>
2020-11-06 19:27:49 +0900

    Final comp

--- 0-everyday/src/App.js
+++ 0-everyday/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode: "read",
+      selected_content_id: 2,
       subject: { title: "web", sub: "World Wide Web" },
       welcome: { title: "welcome", desc: "Hello React" },
       contents: [
@@ -25,8 +26,16 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === "read") {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      let i = 0;
+      while (i < this.state.contents.length) {
+        let data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
 
     return (
@@ -38,7 +47,12 @@ class App extends Component {
             this.setState({ mode: "welcome" });
           }.bind(this)}
         />
-        <TOC data={this.state.contents}></TOC>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
         <Content title={_title} desc={_desc} />
       </div>
     );
