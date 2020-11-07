08edd7d3f05098cd50655f4190b20d9186f3a194
iDeveloperBrandon <hsy0602@outlook.com>
2020-11-07 17:21:02 +0900

    Create content with concat()

--- 0-everyday/src/App.js
+++ 0-everyday/src/App.js
@@ -10,6 +10,7 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode: "create",
       selected_content_id: 2,
@@ -47,6 +48,16 @@ class App extends Component {
         <CreateContent
           onSubmit={function (_title, _desc) {
             // add content to this.state.contents
+            this.max_content_id = this.max_content_id + 1;
+            let _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+            });
+            console.log(_title, _desc);
           }.bind(this)}
         ></CreateContent>
       );
