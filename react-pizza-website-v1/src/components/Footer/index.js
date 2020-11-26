d6be8753bcfce653a755b135c003f81f00d09bcc
iDeveloperBrandon <hsy0602@outlook.com>
2020-11-26 19:05:21 +0900

    Footer

---
+++ react-pizza-website-v1/src/components/Footer/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink,
+} from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Pizza</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink
+                href="/"
+                target="_blank"
+                aria-label="Facebook"
+                rel="noopener"
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blank"
+                aria-label="Instagram"
+                rel="noopener"
+              >
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blank"
+                aria-label="YouTube"
+                rel="noopener"
+              >
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blank"
+                aria-label="Twitter"
+                rel="noopener"
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blank"
+                aria-label="Linkedin"
+                rel="noopener"
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
