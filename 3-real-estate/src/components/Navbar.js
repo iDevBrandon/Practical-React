879c0c1a81d95da5e61de206fcffdc90aae7c50e
Brandon Ha <hsy0602@outlook.com>
2020-12-28 01:58:09 +0900

    Navbar setting without CSS

---
+++ 3-real-estate/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = styled.nav`
+  height: 60px;
+  background: red;
+`;
+
+const Logo = styled(Link)`
+  color: #fff;
+`;
+
+const MenuBars = styled.i;
+
+const NavMenu = styled.div;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Logo>RoboTaxi</Logo>
+      <MenuBars />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
+};
+
+export default Navbar;
