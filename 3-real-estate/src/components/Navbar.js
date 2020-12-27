f482b56aca853e5fbe51b6ead038cf9c7d5cd0f9
Brandon Ha <hsy0602@outlook.com>
2020-12-28 03:03:23 +0900

    NavMenu component

--- 3-real-estate/src/components/Navbar.js
+++ 3-real-estate/src/components/Navbar.js
@@ -1,26 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { menuData } from "../data/MenuData";
 
 const Nav = styled.nav`
   height: 60px;
-  background: red;
+  background: #000; ;
 `;
 
 const Logo = styled(Link)`
   color: #fff;
 `;
 
-const MenuBars = styled.i;
+const MenuBars = styled.i``;
 
-const NavMenu = styled.div;
+const NavMenu = styled.div``;
+
+const NavMenuLinks = styled(Link)`
+  color: #fff;
+`;
 
 const Navbar = () => {
   return (
     <Nav>
-      <Logo>RoboTaxi</Logo>
+      <Logo to="/">RoboTaxi</Logo>
       <MenuBars />
-      <NavMenu></NavMenu>
+      <NavMenu>
+        {menuData.map((item, index) => {
+          return (
+            <NavMenuLinks to={item.link} key={index}>
+              {item.title}
+            </NavMenuLinks>
+          );
+        })}
+      </NavMenu>
     </Nav>
   );
 };
