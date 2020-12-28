cdc26fc84c2eda0a62c8d1fcc82f5b8eb5f127ee
Brandon Ha <hsy0602@outlook.com>
2020-12-28 17:10:22 +0900

    Navbar component styling

--- 3-real-estate/src/components/Navbar.js
+++ 3-real-estate/src/components/Navbar.js
@@ -1,23 +1,50 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
 
 const Nav = styled.nav`
   height: 60px;
-  background: #000; ;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red; ;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
 `;
 
 const Logo = styled(Link)`
-  color: #fff;
+  ${NavLink}
+  font-style: italic;
 `;
 
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+`;
 
 const NavMenuLinks = styled(Link)`
-  color: #fff;
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
 `;
 
 const Navbar = () => {
@@ -26,14 +53,17 @@ const Navbar = () => {
       <Logo to="/">RoboTaxi</Logo>
       <MenuBars />
       <NavMenu>
-        {menuData.map((item, index) => {
-          return (
-            <NavMenuLinks to={item.link} key={index}>
-              {item.title}
-            </NavMenuLinks>
-          );
-        })}
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
       </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary="true">
+          Contact Us
+        </Button>
+      </NavBtn>
     </Nav>
   );
 };
