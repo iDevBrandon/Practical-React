9d9fc699ed320502006e85b81cda32b7434b25a4
Brandon Ha <hsy0602@outlook.com>
2020-12-28 18:39:06 +0900

    Hero sections images

---
+++ 3-real-estate/src/data/SliderData.js
@@ -0,0 +1,69 @@
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in Toronto, Canada",
+    price: "$2,860,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "House in Califonia, CA",
+    price: "$12,460,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Dallas, TX",
+    price: "$6,660,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+
+export const SliderDataTwo = [
+  {
+    title: "House in Las Vegas, Nevada",
+    price: "$6,310,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFive,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$7,760,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageSix,
+    alt: "House",
+  },
+  {
+    title: "House in Melbune, Austin",
+    price: "$2,2360,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageSeven,
+    alt: "House",
+  },
+  {
+    title: "House in Costa Rito",
+    price: "$3,240,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageEight,
+    alt: "House",
+  },
+];
