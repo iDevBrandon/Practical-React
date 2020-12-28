9c547dd1301a884fabc11477b41bb2d6ab3758f7
Brandon Ha <hsy0602@outlook.com>
2020-12-28 20:32:19 +0900

    Creating Hero - HeroSilder & putting ArrowButtons

--- 3-real-estate/src/data/SliderData.js
+++ 3-real-estate/src/data/SliderData.js
@@ -1,3 +1,12 @@
+import ImageOne from "../images/house-1.jpg";
+import ImageTwo from "../images/house-2.jpg";
+import ImageThree from "../images/house-3.jpg";
+import ImageFour from "../images/house-4.jpg";
+import ImageFive from "../images/house-5.jpg";
+import ImageSix from "../images/house-6.jpg";
+import ImageSeven from "../images/house-7.jpg";
+import ImageEight from "../images/house-8.jpg";
+
 export const SliderData = [
   {
     title: "Luxury Villa in Bali, Indonesia",
