64839f2f2464bcd248bc9c6aad7ee0ae75758f0c
Brandon Ha <hsy0602@outlook.com>
2021-03-17 18:10:18 +0900

    project start

---
+++ 6-accordion/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Question from "./Question";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Accordion project setup</h2>
+      <Question />
+    </div>
+  );
+};
+
+export default App;
---
+++ 6-accordion/src/Question.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = () => {
+  return <div>question component</div>;
+};
+
+export default Question;
