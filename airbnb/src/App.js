9cbda1b3d3331e2503b27083bda8e79afe735879
Brandon Ha <hsy0602@outlook.com>
2021-03-24 19:42:36 +0900

    airbnb project setup

---
+++ airbnb/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* Home */}
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* SearchPage */}
+    </div>
+  );
+}
+
+export default App;
