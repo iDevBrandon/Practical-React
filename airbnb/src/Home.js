90198a38650db09d67a4d81ce1771a7dea969609
Brandon Ha <hsy0602@outlook.com>
2021-03-24 23:38:24 +0900

    Banner component

--- airbnb/src/Home.js
+++ airbnb/src/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Banner from "./Banner";
 import "./Home.css";
 
 function Home() {
-  return <div className="Home">{/* <h1>Home component</h1> */}</div>;
+  return (
+    <div className="Home">
+      <h1>Home comp</h1>
+      <Banner />
+    </div>
+  );
 }
 
 export default Home;
