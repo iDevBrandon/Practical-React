6edc550b74724021017c986fd3f083b42acdfb8d
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-03-27 19:12:26 +0900

    add todos func

--- firetodo/src/App.js
+++ firetodo/src/App.js
@@ -1,9 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Take dogs for a walk",
+    "Take the rubbish out",
+    "i am idev",
+  ]);
+  const [input, setInput] = useState("");
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    event.preventDefault();
+    console.log("clicked me");
+
+    setTodos([...todos, input]);
+    setInput(""); // clear up the input
+  };
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
