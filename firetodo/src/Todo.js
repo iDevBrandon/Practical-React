1f2c59308c22fce68e6d3cab9e3aeb6bc9f9d539
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-03-28 01:48:59 +0900

    create a modal

--- firetodo/src/Todo.js
+++ firetodo/src/Todo.js
@@ -1,22 +1,37 @@
-import { Button, List, ListItem, ListItemText } from "@material-ui/core";
+import { List, ListItem, ListItemText, Modal } from "@material-ui/core";
 import "./Todo.css";
-import React from "react";
+import React, { useState } from "react";
 import db from "./firebase";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
 function Todo({ todo }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <List className="todo__list">
-      <ListItem>
-        <ListItemText
-          primary={todo.todo}
-          secondary="Dummy deadline"
-        ></ListItemText>
-      </ListItem>
-      <DeleteForeverIcon
-        onClick={(event) => db.collection("todos").doc(todo.id).delete()}
-      />
-    </List>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div>
+          <h1>I am a modal</h1>
+          <button onClick={(e) => setOpen(false)}></button>
+        </div>
+      </Modal>
+      <List className="todo__list">
+        <ListItem>
+          <ListItemText
+            primary={todo.todo}
+            secondary="Dummy deadline"
+          ></ListItemText>
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>Edit</button>
+        <DeleteForeverIcon
+          onClick={(event) => db.collection("todos").doc(todo.id).delete()}
+        />
+      </List>
+    </>
   );
 }
 
