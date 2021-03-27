88e0d83abec44e83a5bc6a1dd5b8980efe09d50c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-03-28 00:12:08 +0900

    Read from firebase

--- firetodo/src/App.js
+++ firetodo/src/App.js
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 import { FormControl, Input, InputLabel } from "@material-ui/core";
 import Todo from "./Todo";
+import db from "./firebase";
 
 function App() {
-  const [todos, setTodos] = useState([
-    "Take dogs for a walk",
-    "Take the rubbish out",
-    "i am idev",
-  ]);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+
+  // when the app loads, we need to listen to the database and fetch new todos as they get added
+  useEffect(() => {
+    // this code here ... fires when the app.js loads
+    db.collection("todos").onSnapshot((snapshot) => {
+      console.log(snapshot.docs.map((doc) => doc.data()));
+      setTodos(snapshot.docs.map((doc) => doc.data().todo));
+    });
+  }, []);
+
   const addTodo = (event) => {
     // this will fire off when we click the button
     event.preventDefault();
