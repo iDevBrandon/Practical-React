023261fb2610ea1dacfa94f33307e1f5ba49bfeb
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-03-28 02:16:08 +0900

    modal styling

--- firetodo/src/Todo.js
+++ firetodo/src/Todo.js
@@ -1,10 +1,28 @@
-import { List, ListItem, ListItemText, Modal } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+  Modal,
+} from "@material-ui/core";
 import "./Todo.css";
 import React, { useState } from "react";
 import db from "./firebase";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function Todo({ todo }) {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -14,7 +32,7 @@ function Todo({ todo }) {
   return (
     <>
       <Modal open={open} onClose={(e) => setOpen(false)}>
-        <div>
+        <div className={classes.paper}>
           <h1>I am a modal</h1>
           <button onClick={(e) => setOpen(false)}></button>
         </div>
