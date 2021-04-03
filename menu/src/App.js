063a58495cc1f95158f0c17eed5d47da3f77b00e
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-04 01:56:27 +0900

    Display images

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,5 +1,23 @@
+import React, { useState } from "react";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+import items from "./data";
+
 function App() {
-  return <div className="App"></div>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
