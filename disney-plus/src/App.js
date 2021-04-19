36734fe9c590eea1b8ce455c7904dd6ed8a4f8f9
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-19 16:05:16 +0900

    NavMenu

--- disney-plus/src/App.js
+++ disney-plus/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <Login />
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
