60aeda8bcd9c7aff4030a072d5a8d8e31e96d84c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-19 13:59:18 +0900

    Login page styling

--- disney-plus/src/App.js
+++ disney-plus/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App">Disney</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
