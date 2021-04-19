d3deaa574cbcdfd73d1753fdf0fec45d17868512
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-19 14:15:32 +0900

    signup component

--- disney-plus/src/components/Login.js
+++ disney-plus/src/components/Login.js
@@ -7,6 +7,7 @@ const Login = (props) => {
         <CTA>
           <CTALogoOne src="/images/cta-logo.png" />
         </CTA>
+        <SignUp>Get all there</SignUp>
         <BgImage />
       </Content>
     </Container>
@@ -71,6 +72,24 @@ const CTALogoOne = styled.img`
   min-height: 1px;
   display: block;
   width: 100%;
+`;
+
+const SignUp = styled.a`
+  text-transform: uppercase;
+  font-weight: 900;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
 `;
 
 export default Login;
