5aa0c497ed721d17c4d1926c14d9192d3cbf6686
Brandon Ha <hsy0602@outlook.com>
2021-04-29 17:10:37 +0900

    funcitonal component

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <h2>What a nice day</h2>
+    </div>
+  );
 }
 
 export default App;
