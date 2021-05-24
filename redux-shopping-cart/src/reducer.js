f4c6db1ae72eb602c9432656ed9030382f5df524
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-24 17:44:05 +0900

    remove reducer

--- redux-shopping-cart/src/reducer.js
+++ redux-shopping-cart/src/reducer.js
@@ -1,16 +1,41 @@
-import { CLEAR_CART } from "./actions";
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./actions";
 
 function reducer(state, action) {
-  switch (action.type) {
-    case CLEAR_CART:
-      return { ...state, cart: [] };
-    default:
-      return state;
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
   }
-  //   if (action.type === CLEAR_CART) {
-  //     return { ...state, cart: [] };
-  //   }
-  //  return state;
+
+  if (action.type === DECREASE) {
+    return {
+      ...state,
+      amount: state.amount - 1,
+    };
+  }
+
+  if (action.type === INCREASE) {
+    return {
+      ...state,
+      amount: state.amount + 1,
+    };
+  }
+
+  if (action.type === REMOVE) {
+    // console.log(action.payload.id);
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
+    };
+  }
+
+  return state;
 }
 
 export default reducer;
+
+// switch example
+// switch (action.type) {
+//   case CLEAR_CART:
+//     return { ...state, cart: [] };
+//   default:
+//     return state;
+// }
