60bf6593938b470582fe60de581834066597316a
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-31 15:32:15 +0900

    Apply Map comp to App

--- nasa-tracker/src/components/Map.js
+++ nasa-tracker/src/components/Map.js
@@ -1,7 +1,23 @@
-import React from "react";
+import GoogleMapReact from "google-map-react";
 
-const Map = () => {
-  return <div></div>;
+const Map = ({ center, zoom }) => {
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDUfYYqsLqCYCLvUEFdAcPFDMUjJ6ptA2o" }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 43.6532,
+    lng: -79.347015,
+  },
+  zoom: 6,
 };
 
 export default Map;
