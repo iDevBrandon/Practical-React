d5516453dc82a24d56858fb760a00f71ba12c7b9
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-31 17:35:53 +0900

    complete

--- nasa-tracker/src/App.js
+++ nasa-tracker/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Map from "./components/Map";
 import Loader from "./components/Loader";
+import Header from "./components/Header";
 
 function App() {
   const [eventData, setEventData] = useState([]);
@@ -26,6 +27,7 @@ function App() {
   return (
     <div>
       <h1>Welcome to the App</h1>
+      <Header />
       {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
