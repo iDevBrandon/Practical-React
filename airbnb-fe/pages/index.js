f60dd8c44e40d3b7fa858f273ca43102c4eee143
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-03 16:14:59 +0900

    Querying our Data with GROQ

--- airbnb-fe/pages/index.js
+++ airbnb-fe/pages/index.js
@@ -1,5 +1,28 @@
-const Home = () => {
+import defineProperties from "define-properties";
+import { sanityClient } from "../sanity";
+
+const Home = ({ properties }) => {
+  console.log(properties);
   return <div></div>;
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[ _type == "property"]';
+  const properties = await sanityClient.fetch(query);
+
+  if (!defineProperties.length) {
+    return {
+      props: {
+        properties: [],
+      },
+    };
+  } else {
+    return {
+      props: {
+        properties,
+      },
+    };
+  }
 };
 
 export default Home;
