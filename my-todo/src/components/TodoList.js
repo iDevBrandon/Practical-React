19d66c262d8a540ea75272b072e284f6cfa1ec04
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-05 22:23:42 +0900

    update

--- my-todo/src/components/TodoList.js
+++ my-todo/src/components/TodoList.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Todo from "./Todo";
-import { TodoListContainer, Todos } from "./TodoList.style";
+import { TodoListContainer } from "./TodoList.style";
 
 const TodoList = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   return (
     <TodoListContainer>
       <h1>What's up?</h1>
-      <Todos>
-        {todos.map((todo) => (
-          <Todo
-            todo={todo}
-            key={todo.id}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
-            updateTodo={updateTodo}
-          />
-        ))}
-      </Todos>
+
+      {todos.map((todo) => (
+        <Todo
+          todo={todo}
+          key={todo.id}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          updateTodo={updateTodo}
+        />
+      ))}
     </TodoListContainer>
   );
 };
