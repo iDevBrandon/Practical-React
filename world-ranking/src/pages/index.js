fd280d4a69111276038d825c689ed630ae19d3f7
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-11 00:22:39 +0900

    Build Input component

--- world-ranking/src/pages/index.js
+++ world-ranking/src/pages/index.js
@@ -1,7 +1,25 @@
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
+import SearchInput from "../components/SearchInput/SearchInput";
 
-export default function Home() {
-  return <Layout>main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+    </Layout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
