7121a6a32f1029dab74e63313fbb52b467ef7894
Brandon Ha <idevbrandon@gmail.com>
2021-07-05 17:59:37 +0900

    Update Home.js

--- linkedin/src/components/Home.js
+++ linkedin/src/components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding-top: 55px;
+  max-width: 100%;
+`;
+
+const Section = styled.section``;
 
 const Home = (props) => {
-  return <div>Homeasdfasfsdfjksdjfkjs</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a href="/home">Hiring in a hurry? - </a>
+        </h5>
+        <p>Find talented pros in upwork and keep business moving forward </p>
+      </Section>
+    </Container>
+  );
 };
 
 export default Home;
