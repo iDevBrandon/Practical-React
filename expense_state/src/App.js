020e7220250bcc872bbc3c84deac556130877930
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-07-07 16:53:08 +0900

    setup

---
+++ expense_state/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- world-ranking/src/pages/index.js
+++ world-ranking/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 import SearchInput from "../components/SearchInput/SearchInput";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 
 export default function Home({ countries }) {
   console.log(countries);
@@ -9,6 +10,7 @@ export default function Home({ countries }) {
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
       <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+      <CountriesTable countries={countries} />
     </Layout>
   );
 }
