381cba334cfd042938697cc3f594f9a7dfdaccb4
Brandon Ha <idevbrandon@gmail.com>
2021-07-11 15:37:00 +0900

    Putting components

---
+++ expense/src/components/AddTransaction.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AddTransaction = () => {
+  return (
+    <div>
+      <h3>Add new Transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - positive, positive - negative)
+          </label>
+          <input type="number" id="amount" placeholder="Enter amount..." />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddTransaction;
