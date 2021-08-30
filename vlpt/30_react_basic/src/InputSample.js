f98e5186d538ee3f4c04791b18a1c600cf34975d
Brandon Ha <idevbrandon@gmail.com>
2021-08-30 21:59:59 +0900

    useRef

--- vlpt/30_react_basic/src/InputSample.js
+++ vlpt/30_react_basic/src/InputSample.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const InputSample = () => {
   const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
+
+  const nameInput = useRef();
 
   const { name, nickname } = inputs;
 
@@ -21,11 +23,18 @@ const InputSample = () => {
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input name="name" type="text" onChange={onChange} value={name} />
+      <input
+        name="name"
+        type="text"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input name="nickname" type="text" onChange={onChange} value={nickname} />
       <button onClick={initHandler}>Submit</button>
       <span>Value:</span>
