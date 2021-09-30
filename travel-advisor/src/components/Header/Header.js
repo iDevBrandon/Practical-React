c407b2f944ce851e3a95c779a632769439adca4d
Brandon Ha <idevbrandon@gmail.com>
2021-09-30 13:46:11 +0900

    Header component

--- travel-advisor/src/components/Header/Header.js
+++ travel-advisor/src/components/Header/Header.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+// import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, InputBase, Box, Typography } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const classes = useStyles();
 
-export default Header
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          OX root
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
